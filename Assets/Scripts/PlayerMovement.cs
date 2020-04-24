0c8c2e378e09f569e8f80dbd228bf4510d8fe26b
lurdan <hastgt@icloud.com>
2020-04-24 21:30:17 +0200

    Player movement. camera follows the player, animations

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float _moveSpeed = 10f;
+    public float _rotationSpeed = 10f;
+
+    public Animator animator;
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Move", Input.GetAxis("Horizontal"));
+        animator.SetFloat("Move", Input.GetAxis("Vertical"));
+        Movement();
+    }
+
+    private void Movement()
+    {
+        float rotate = Input.GetAxis("Horizontal") * _rotationSpeed;
+        float moveVertical = Input.GetAxis("Vertical") * _moveSpeed;
+        rotate *= Time.deltaTime;
+        moveVertical *= Time.deltaTime;
+        transform.Rotate(0, 0, rotate);
+        transform.Translate(0, moveVertical, 0);
+    }
+}
