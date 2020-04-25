6d4081dbca633e3aa3619e24828ff41f79e9dc30
SG Benjamin Rosenberger <benjamin.rosenberger@softgames.de>
2020-04-25 18:03:00 +0200

    fix step sound and player prefab

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,42 +10,63 @@ public class PlayerMovement : MonoBehaviour
     bool isColliding = false;
 
     public Animator animator;
+    AudioManager audioManager;
 
-  
+    float walkSoundTimer = 0;
+    public float walkSoundTime = 0.3f;
+
+    private void Start()
+    {
+        audioManager = FindObjectOfType<AudioManager>();
+    }
+
     // Update is called once per frame
     void Update()
     {
         //animator.SetFloat("Move", Input.GetAxis("Horizontal"));
         //animator.SetFloat("Move", Input.GetAxis("Vertical"));
-        Movement();
+        bool isMoving = Movement();
+        walkSoundTimer += Time.deltaTime;
+        if(walkSoundTimer > walkSoundTime)
+        {
+            walkSoundTimer = 0;
+            if (isMoving)
+            {
+                audioManager.Play("Walking");
+            }
+        }
     }
 
 
-        private void Movement()
+    private bool Movement()
     {
-        FindObjectOfType<AudioManager>().Play("Walking");
         animator.SetBool("isPressed", false);
+        bool isMoving = false;
        
         if (Input.GetKey(KeyCode.A))
         {
+            isMoving = true;
             animator.SetBool("isPressed", true);
             transform.position += Vector3.left * _moveSpeed * Time.deltaTime;
         }
         
         if (Input.GetKey(KeyCode.D))
         {
+            isMoving = true;
             animator.SetBool("isPressed", true);
             transform.position += Vector3.right * _moveSpeed * Time.deltaTime;
         }
        
         if (Input.GetKey(KeyCode.W))
         {
+            isMoving = true;
             animator.SetBool("isPressed", true);
             transform.position += Vector3.up * _moveSpeed * Time.deltaTime;
         }
        
         if (Input.GetKey(KeyCode.S))
         {
+            isMoving = true;
             animator.SetBool("isPressed", true);
             transform.position += Vector3.down * _moveSpeed * Time.deltaTime;
         }
@@ -61,6 +82,8 @@ public class PlayerMovement : MonoBehaviour
         Vector2 lookatCamera = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         float rotationZ = Mathf.Atan2(lookatCamera.normalized.y, lookatCamera.normalized.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, rotationZ - 90);
+
+        return isMoving;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
