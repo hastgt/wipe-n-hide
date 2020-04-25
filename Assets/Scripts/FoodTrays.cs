472ed4bbedbb5bc81714819a3e0383a9abd3485f
lurdan <hastgt@icloud.com>
2020-04-25 13:44:38 +0200

    added food trays

--- Assets/Scripts/FoodTrays.cs
+++ Assets/Scripts/FoodTrays.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class FoodTrays : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private bool _isPickable;
+    public Transform food;
+    public Transform hands;
+
+    private void Update()
     {
-        
+        PickingUp();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void PickingUp()
     {
-        
+        if (_isPickable = true && Input.GetKey(KeyCode.E))
+        {
+            food.transform.position = hands.transform.position;
+            Vector2 lookatCamera = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            float rotationZ = Mathf.Atan2(lookatCamera.normalized.y, lookatCamera.normalized.x) * Mathf.Rad2Deg;
+            food.rotation = Quaternion.Euler(0f, 0f, rotationZ - 90);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("true");
+        _isPickable = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("false");
+        _isPickable = false;
     }
 }
