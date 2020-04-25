ec22a74629b886b15b4ce5157ccc7735b5184e36
SG Benjamin Rosenberger <benjamin.rosenberger@softgames.de>
2020-04-25 19:02:01 +0200

    title screen

---
+++ Assets/Pulse.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pulse : MonoBehaviour
+{
+    public float timeMultiplier = 3f;
+
+    [Range(0f, 1f)]
+    public float amplitude = 0.3f;
+
+    void Update()
+    {
+        float scale = 1 + amplitude * Mathf.Sin(Time.time * timeMultiplier);
+        this.transform.localScale = new Vector3(scale, scale, scale);
+    }
+}
---
+++ Assets/Wobble.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wobble : MonoBehaviour
+{
+    public float timeMultiplier = 0.5f;
+    public float maxAngle = 10f;
+
+    void Update()
+    {
+        this.transform.localEulerAngles = new Vector3(0, 0, Mathf.Sin(Time.time * timeMultiplier) * maxAngle);
+    }
+}
