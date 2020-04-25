ee1a45ca432a7be9f27a3503370fdd39f573859a
lurdan <hastgt@icloud.com>
2020-04-25 12:31:19 +0200

    updated player controller

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,22 +5,51 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float _moveSpeed = 10f;
-    public float _rotationSpeed = 10f;
+    //public float _rotationSpeed = 10f;
 
     public Animator animator;
+
     // Update is called once per frame
     void Update()
     {
-        animator.SetFloat("Move", Input.GetAxis("Horizontal"));
-        animator.SetFloat("Move", Input.GetAxis("Vertical"));
+        //animator.SetFloat("Move", Input.GetAxis("Horizontal"));
+        //animator.SetFloat("Move", Input.GetAxis("Vertical"));
         Movement();
+
     }
 
     private void Movement()
     {
-        float moveVertical = Input.GetAxis("Vertical") * _moveSpeed;
-        moveVertical *= Time.deltaTime;
-        transform.Translate(0, moveVertical, 0);
+        animator.SetBool("isPressed", false);
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            animator.SetBool("isPressed", true);
+            transform.position += Vector3.left * _moveSpeed * Time.deltaTime;
+        }
+        
+        if (Input.GetKey(KeyCode.D))
+        {
+            animator.SetBool("isPressed", true);
+            transform.position += Vector3.right * _moveSpeed * Time.deltaTime;
+        }
+       
+        if (Input.GetKey(KeyCode.W))
+        {
+            animator.SetBool("isPressed", true);
+            transform.position += Vector3.up * _moveSpeed * Time.deltaTime;
+        }
+       
+        if (Input.GetKey(KeyCode.S))
+        {
+            animator.SetBool("isPressed", true);
+            transform.position += Vector3.down * _moveSpeed * Time.deltaTime;
+        }
+       
+
+        //float moveVertical = Input.GetAxis("Vertical") * _moveSpeed;
+        //moveVertical *= Time.deltaTime;
+        //transform.Translate(0, moveVertical, 0);
         //float rotate = Input.GetAxis("Horizontal") * _rotationSpeed;
         //rotate *= Time.deltaTime;
         //transform.Rotate(0, 0, rotate);
