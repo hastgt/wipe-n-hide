5bf9c22a0edfb391898636a22a67614f0d9591b0
SG Benjamin Rosenberger <benjamin.rosenberger@softgames.de>
2020-04-26 14:57:31 +0200

    tilting camera

--- Assets/Scripts/TiltAround.cs
+++ Assets/Scripts/TiltAround.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class TiltAround : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float tiltTime = 2f;
+    public float tiltDuration = 1f;
+    public float tiltAngle = 30f;
+    bool isTiltedLeft = true;
+    bool isTilting = false;
+    float tiltTimer = 0f;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        tiltTimer += Time.deltaTime;
+        if(tiltTimer >= tiltTime + tiltDuration)
+        {
+            tiltTimer = 0f;
+            StartCoroutine(StartTilt());
+        }
     }
+
+    IEnumerator StartTilt()
+    {
+        isTilting = true;
+        bool hasReachedLimit = false;
+
+        while (!hasReachedLimit)
+        {
+            float tiltAngleDir = isTiltedLeft ? 1 : -1;
+            transform.localEulerAngles += new Vector3(0, 0, tiltAngleDir * tiltAngle * Time.deltaTime / tiltDuration);
+
+            print((360-transform.localEulerAngles.z) % 360);
+
+            hasReachedLimit = transform.localEulerAngles.z > tiltAngle;
+            
+            yield return new WaitForEndOfFrame();
+        }
+        transform.localEulerAngles = new Vector3(0, 0, isTiltedLeft ? tiltAngle : 0f);
+        isTiltedLeft = !isTiltedLeft;
+        isTilting = false;
+
+    }
+
 }
